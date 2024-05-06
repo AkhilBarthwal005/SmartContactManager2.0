a6b00c46dc1c34d4d61c4a5c91a18eb21669f7fa
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2024-05-06 13:10:37 +0530

    initial project setup

---
+++ src/main/java/com/scm/controller/Welcome.java
@@ -0,0 +1,13 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Welcome {
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+}
