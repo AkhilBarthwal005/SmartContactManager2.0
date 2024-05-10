259c0dd85ea7adeec10bab0c3c503c4a99f8c4fb
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2024-05-10 10:50:16 +0530

    Navbar created

--- src/main/java/com/scm/controller/Welcome.java
+++ src/main/java/com/scm/controller/Welcome.java
@@ -1,13 +1,33 @@
 package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/scm")
 public class Welcome {
 
-    @RequestMapping("/welcome")
-    public String welcome() {
-        return "welcome";
+    @GetMapping("")
+    public String home() {
+        return "home";
     }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    } @GetMapping("/service")
+    public String service() {
+        return "service";
+    } @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    } @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    } @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
 }
